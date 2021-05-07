5fc9d5a336218b3d51e07665e790fde90c301103
torjohnson <81002067+torjohnson@users.noreply.github.com>
2021-05-07 13:12:11 +0400

    Commiot

--- data_in.py
+++ data_in.py
@@ -8,18 +8,19 @@ def create_radar():
     radar_single = []
 
     radar1 = requests.get('https://www.geog.leeds.ac.uk/courses/computing/study/core-python-odl2/assessment2/white1.radar')
-    content = radar1 .text
+    type(radar1)
+    content = radar1.text
 
-    for row in radar1 :                     # A list of rows
+    for row in content:                     # A list of rows
         rowlist = []
         for value in row:				    # A list of value
             rowlist.append(value)           # Append values to rowlist
             # print(value) 				    # Prints floats
         radar_single.append(rowlist)     # Append rowlist to environment
 
-    file.close()
-    return float(radar_single)
     print(radar_single)
+    type(radar_single)
+    return (radar_single)
 
 # read in lidar data (single iceberg)
 def create_lidar():
@@ -28,12 +29,13 @@ def create_lidar():
     lidar1 = requests.get('https://www.geog.leeds.ac.uk/courses/computing/study/core-python-odl2/assessment2/white1.lidar')
     content = lidar1.text
 
-    for row in lidar1:                     # A list of rows
+    for row in content:                     # A list of rows
         rowlist = []
         for value in row:				    # A list of value
             rowlist.append(value)           # Append values to rowlist
             # print(value) 				    # Prints floats
         lidar_single.append(rowlist)     # Append rowlist to environment
 
-    file.close()
-    return float(lidar_single)
+    return (lidar_single)
+
+create_radar
