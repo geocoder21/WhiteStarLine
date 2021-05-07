6b0657b0be4cbdfe6f48ea36ea6211112839ba1f
torjohnson <81002067+torjohnson@users.noreply.github.com>
2021-05-07 12:16:16 +0400

    Updates

--- icebergclass.py
+++ icebergclass.py
@@ -1,6 +1,6 @@
 # define iceberg class
 class Iceberg():
-    """Icebergs have coordinates (pixel square), height in m, mass if kg, and volume in m3. 
+    """Icebergs have coordinates (1m? pixel square), height in m, mass if kg, and volume in m3. 
     Those less than 36mil kg mass can be pulled by an iceberg tug"""
 
     def __init__(self, y=none, x=none, height, volume, pull):
