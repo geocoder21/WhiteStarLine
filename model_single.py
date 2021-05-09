ae0a6c3acad480ddae5078ea9617bf0d093ae8dc
torjohnson <81002067+torjohnson@users.noreply.github.com>
2021-05-09 14:09:47 +0400

    Updated GUI

--- model_single.py
+++ model_single.py
@@ -1,7 +1,10 @@
 # IMPORTS
+import tkinter 
+import numpy as np
 import matplotlib 
 import matplotlib.pyplot as plt
-import tkinter 
+# import seaborn as sns
+
 # import icebergclass
 import data_IO
 
@@ -34,6 +37,10 @@ def ice_pull(mass):
 # ******************************************************************************************************************************
 # MAIN PROGRAMME
 # ******************************************************************************************************************************
+
+# set up backend correctly before fig is created
+matplotlib.use('TkAgg')
+# sns.set_theme()
 
 # IMPORT RADAR AND LIDAR DATA
 
@@ -71,14 +78,40 @@ if ice_pull(total_mass) == True:
 else:
     print("Iceberg is too large to be towed")
 
-# 5. DISPLAY total mass, total volume and whether iceberg can be pulled on GUI
+# DISPLAY total mass, total volume and whether iceberg can be pulled on GUI
 
-# plt.ylim(0, 300)                        # y dimension limit
-# plt.xlim(0, 300)                        # x dimension limit
-# plt.imshow(new_radar)
+# GUI code
+root = tkinter.Tk()
+root.wm_title("Iceberg tow model")
+
+# create a new figure
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+# ax.set_autoscale_on(False)
+
+# make a canvas
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+# add text widget to display data
+text = tkinter.Text(root, height=4, width=40, fg='navy')
+text.pack()
+text.insert(tkinter.END, "Total iceberg mass = " + str(total_mass) + " kg")
+text.insert(tkinter.END, '\n')
+text.insert(tkinter.END, "Total iceberg volume = " + str(total_mass/900) + " m3")
+text.insert(tkinter.END, '\n')
+text.insert(tkinter.END, '\n')
+if ice_pull(total_mass) == True:
+    text.insert(tkinter.END,"Iceberg can be towed")
+else:
+    text.insert(tkinter.END,"Iceberg is too large to be towed")
+
+plt.ylim(0, 300)                        # y dimension limit
+plt.xlim(0, 300)                        # x dimension limit
+plt.imshow(new_radar, cmap="plasma")
 # plt.imshow(new_lidar)
-# plt.show()
+canvas.draw()
 
-
+tkinter.mainloop()
 
 # SAVE INFORMATION TO A FILE
